16744b7a50e2187f3e79887b637ee1badae0ed22
mimuki <60542090+mimuki@users.noreply.github.com>
2023-09-01 10:51:56 +0930

    Default to previous posts' CW when replying

--- post.sh
+++ post.sh
@@ -3,12 +3,25 @@ REPLYID=$2
 
 # -n = if the length of the string is non-zero (you pressed the reply binding)
 if [ -n "$REPLYID" ]
+then
+  # awk to check if the status has a CW
+  # then cut off the "cw: " part if it does
+  CW="$(awk "/^status id: $REPLYID/ {f=1} f && /^-----/ {exit} f && /cw:/ {print; exit}" $HOME/.config/msync/msync_accounts/$ACCOUNT/home.list | cut -c 5-)"
+
+  # TODO: this but good
+  if [ -n "$CW" ]
   then
-    msync generate --reply-to $REPLYID
+    msync generate --reply-to $REPLYID --content-warning "$CW"
   else
-    msync generate 
+    msync generate --reply-to $REPLYID
+  fi
+else
+  msync generate 
 fi
-vim "new_post"
+# Jump to end of file, ready to write a post
+# caveat: you need to q! even if you haven't done anything else,
+# since by adding a new line it modifies the file
+vim -c 'execute "normal GGo"'  new_post
 
 # -s = if the file exists and has a non-zero size
 if [ -s "new_post" ]
